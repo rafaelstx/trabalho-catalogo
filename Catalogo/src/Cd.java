f1257299a66a8afc97720257c0bc048ab412a851
rafaelstx <rafaelsteixeira03@gmail.com>
2022-05-28 14:37:15 -0300

    media part

---
+++ Catalogo/src/Cd.java
@@ -0,0 +1,19 @@
+public class Cd extends Media {
+    private int tracks;
+
+    public Cd {
+
+    }
+
+    public Cd(int tracks) {
+        this.tracks = tracks;
+    }
+
+    public int getTracks() {
+        return this.tracks;
+    }
+
+    public void setTracks(int tracks) {
+        this.tracks = tracks;
+    }
+}
